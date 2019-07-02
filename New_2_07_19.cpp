dc9be824c94a35fdcda550610f3904d8ebd6f779
AdrianaTroncosoAuronix <52461455+AdrianaTroncosoAuronix@users.noreply.github.com>
2019-07-02 13:20:42 -0500

    Otro hola mundo con comments

--- New_2_07_19.cpp
+++ New_2_07_19.cpp
@@ -1,2 +1,20 @@
 # ZweitesRepository
 #Segundo sin read me :v
+#include <stdio.h>
+#include <cstdlib>
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	string str = "Hola mundo!";
+    std::string::iterator it;
+    std::string::reverse_iterator it1;
+
+
+
+    for (it=str.begin(); it!=str.end(); it++)
+    cout << *it;
+    cout << endl;
+}
